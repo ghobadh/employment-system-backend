7db4582ce421802b51993c5305508197f744766c
ghobadh <ghobadh@gmail.com>
2024-11-15 16:34:28 -0500

    Initial commit

---
+++ src/main/java/ca/gforcesoftware/employmentsystembackend/EmploymentSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package ca.gforcesoftware.employmentsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploymentSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmploymentSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/gforcesoftware/employmentsystembackend/EmploymentSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gforcesoftware.employmentsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmploymentSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
